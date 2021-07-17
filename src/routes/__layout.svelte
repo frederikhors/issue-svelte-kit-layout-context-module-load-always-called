222f2771ec32d0ee6faef7e6ec8131d10d2cabfc
frederikhors <41120635+frederikhors@users.noreply.github.com>
2021-07-17 13:55:28 +0200

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,54 @@
+<script context="module" lang="ts">
+	import type { LoadInput, LoadOutput } from '@sveltejs/kit';
+
+	export function load({ page }: LoadInput): LoadOutput {
+		console.log(`I'm the load func in context="module" of routes/__layout for page:${page.path}`);
+		return {};
+	}
+</script>
+
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer>
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
